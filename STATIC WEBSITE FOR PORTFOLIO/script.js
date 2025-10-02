7247c15099f41c0749fbb7fde0ee456c00e98b31
Mythili <mythiliarivazhagan19@gmail.com>
2025-10-02 18:42:24 +0530

    Add files via upload

---
+++ STATIC WEBSITE FOR PORTFOLIO/script.js
@@ -0,0 +1,5 @@
+// Simple form submission alert
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});
